4961c0801c212aab7003d903d7c584aeaf905755
Wojtek Codes Today <94251112+WojtekCodesToday@users.noreply.github.com>
2022-05-07 19:14:24 +0200

    1.0

---
+++ main.js
@@ -0,0 +1,4 @@
+ext._shutdown = function() {};ext._getStatus = function() { 
+return {status: 2, msg: 'Ready'};}; 
+var descriptor = { blocks: [ ["h","( Hat)","Loop"],],menus: {},
+url: 'https://kyleplo.github.io/scratch-extension-creator/help#'Hello!};ScratchExtensions.register('RoomBox M8', descriptor, ext);
