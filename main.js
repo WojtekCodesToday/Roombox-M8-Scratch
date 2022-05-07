c041703ea2c91d7f70a67781c90b9a87b97870b2
Wojtek Codes Today <94251112+WojtekCodesToday@users.noreply.github.com>
2022-05-07 19:45:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,26 @@
-class RoomBoxM8 {
-    getInfo() {
-        return {
-            "id": "RoomBoxM8",
-            "name": "RoomBoxM8",
-            "blocks": [{
-                    "opcode": "substringy",
-                    "blockType": "reporter",
-                    "text": "letters [num1] through [num2] of [string]",
-                    "arguments": {
-                        "num1": {
-                            "type": "number",
-                            "defaultValue": "2"
-                        },
-                        "num2": {
-                            "type": "number",
-                            "defaultValue": "5"
-                        },
-                        "string": {
-                            "type": "string",
-                            "defaultValue": "hello world"
-                        }
-                    }
-                },
-            }],
-        "menus": { //we will get back to this in a later tutorial
+//USING A JAPENEESE WEBSITE TO MAKE A EXTENSION
+
+class Test {
+
+  constructor() {}
+
+  getInfo() { // 拡張機能の各種情報
+    return {
+      id: 'test',
+      name: 'Test', // 拡張機能の名前
+      blocks: [ // 各ブロックの定義
+        {
+          opcode: 'hello', // このブロックが実行されると、helloという関数が呼ばれる
+          blockType: Scratch.BlockType.COMMAND,　// 「10歩動かす」のような通常の命令ブロック
+          text: 'hello' // ブロックに表示されるテキスト
         }
-    };
+      ]
+    }
+  }
+
+  hello() {
+    window.alert('hello'); // console log に hello と出力
+  }
 }
-Scratch.extensions.register(new RoomBoxM8());
+
+Scratch.extensions.register(new Test());
